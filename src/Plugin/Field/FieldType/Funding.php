af5aaf68a52a52df89cde2089706191faae67f2a
Ryan Price <ryan.price@ffwagency.com>
2022-09-28 00:52:41 -0700

    Issue #3272821: Funding: create field type

---
+++ src/Plugin/Field/FieldType/Funding.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\funding\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\FieldItemBase;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Plugin implementation of the 'funding' field type.
+ *
+ * @FieldType(
+ *   id = "funding",
+ *   label = @Translation("Funding YAML"),
+ *   module = "funding",
+ *   description = @Translation("Accepts YAML describing crowdfunding accounts."),
+ *   default_widget = "funding",
+ *   default_formatter = "funding_text"
+ * )
+ */
+class Funding extends FieldItemBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
+    return [
+      'columns' => [
+        'value' => [
+          'type' => 'text',
+          'size' => 'big',
+          'not null' => FALSE,
+        ],
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEmpty() {
+    $value = $this->get('value')->getValue();
+    return $value === NULL || $value === '';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    $properties['value'] = DataDefinition::create('string')
+      ->setLabel(t('Funding YAML value'));
+
+    return $properties;
+  }
+
+}
